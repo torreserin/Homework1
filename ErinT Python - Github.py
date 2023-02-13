633d14a4b62aacce9cba67cfe9102fb167590068
torreserin <124105350+torreserin@users.noreply.github.com>
2023-02-13 12:07:49 -0500

    Add files via upload

---
+++ ErinT Python - Github.py
@@ -0,0 +1,13 @@
+#Erin Torres Code
+
+monthlyIncome = 0
+
+#Ask user monthly income
+monthlyIncome = input('What is your monthly net income:\n>>')
+
+#Calculate monthly savings amount
+savings = .20*float(monthlyIncome)
+
+#Print monthly income and savings based on input
+incomeSavings = print('Based on your monthly income of', monthlyIncome, 'your savings for the month should be', savings)
+
